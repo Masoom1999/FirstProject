428077071327d69c625f7d5cf29e40447245b755
Masoom1999 <49523423+Masoom1999@users.noreply.github.com>
2019-07-02 00:17:45 +0530

    Update FinalProg1.java

--- FinalProg1.java
+++ FinalProg1.java
@@ -1,55 +1,49 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-class C1 extends Thread
+class A1 extends Thread
 {
+	public static int b;
 	public void run(){
 	try{
 		FileInputStream fis = new FileInputStream("/home/masoom/workspace1/MyProj1/abc.txt");
 		FileOutputStream fos = new FileOutputStream("/home/masoom/workspace1/MyProj1/test");
-	    int b = fis.read();
+		b = fis.read();
 		
 		while(b != -1){
 			fos.write(b);				
 			b = fis.read();
 		}
-		b=0;
 		fis.close();
 		fos.close();
-		//System.out.println("Okay Byee!!");
+		
 	}
 	
 	catch (Exception e) {
 		System.out.println(e.getMessage());
+		System.exit(0);
 	}
 	}
 }
 
 
-class C2 extends Thread
+class A2 extends Thread
 {
 	public void run(){
-		for(int i=0; i<10 ; i++){
+			while(A1.b != -1){
 			System.out.println("*");
+			
 			try {Thread.sleep(1000);} 
 			catch(InterruptedException e) {e.printStackTrace();}
 		}		
 	}
-		
-}
-public class FinalProg1 {
-
 	public static void main(String[] args) {
-		C1 obj1 = new C1();
-		C2 obj2 = new C2();
+		A1 obj1 = new A1();
+		A2 obj2 = new A2();
 		
 		obj1.start();
-		obj2.start();			
-
+		obj2.start();
 	}
+		
 }
-
-
-
-
 
